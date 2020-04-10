69bbe2a0fc849058943f9ed398deecba435a365e
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-04-10 08:25:03 -0400

    tweaks

--- makemax.py
+++ makemax.py
@@ -3,15 +3,19 @@ import glob
 from spectral_cube import SpectralCube
 from spectral_cube.dask_spectral_cube import DaskSpectralCube
 
+import time
 
-for suffix in ( "image.pbcor.fits"):
+for suffix in ("image.pbcor.fits",):
     for fn in glob.glob(f"BrickMaser*{suffix}"):
+        t0 = time.time()
         if not os.path.exists(f'spectra/{fn.replace("{suffix}","max.fits")}'):
-            cube = SpectralCube.read(fn)
+            #cube = SpectralCube.read(fn)
+            #print(cube)
+            #mxspec = cube.max(axis=(1,2), how='slice', progressbar=True)
+            print(fn)
+            cube = DaskSpectralCube.read(fn.replace(".fits", ""), format='casa_image')
             print(cube)
-            mxspec = cube.max(axis=(1,2), how='slice', progressbar=True)
-            #print(fn)
-            #cube = DaskSpectralCube.read(fn.replace(".fits", ""), format='casa_image')
-            #mxspec = cube.max(axis=(1,2))
+            mxspec = cube.max(axis=(1,2))
             mxspec.write(f'spectra/{fn.replace("{suffix}","max.fits")}',
                          overwrite=True)
+            print(time.time() - t0)
