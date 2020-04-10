130951bd385a6cb90bb3f281c00ee01d0ccc8154
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-04-10 11:16:21 -0400

    tweaks

--- full_spectrum_plot.py
+++ full_spectrum_plot.py
@@ -1,7 +1,10 @@
 import glob
 import pyspeckit
+import pylab as pl
+
+pl.figure(figsize=(25,10))
 
 spectra = pyspeckit.Spectra(pyspeckit.Spectrum(x) for x in glob.glob("spectra/*max.fits"))
 spectra.plotter()
 
-spectra.plotter.figure.savefig('full_spectrum_max.png')
+spectra.plotter.figure.savefig('full_spectrum_max.png', dpi=200, bbox_inches='tight')
